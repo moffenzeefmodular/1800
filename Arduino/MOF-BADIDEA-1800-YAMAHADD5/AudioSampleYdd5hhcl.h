ef1caa0f93e0077c43603154d1f357267797f9c0
Møffenzeef Mødular <rossandrewfish@gmail.com>
2020-06-30 16:46:27 -0700

    Add files via upload

---
+++ Arduino/MOF-BADIDEA-1800-YAMAHADD5/AudioSampleYdd5hhcl.h
@@ -0,0 +1,3 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleYdd5hhcl[481];
---
+++ Arduino/MOF-BADIDEA-1800-YAMAHADD5/AudioSampleYdd5hhop.h
@@ -0,0 +1,3 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleYdd5hhop[2017];
